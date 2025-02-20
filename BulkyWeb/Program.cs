c6f36b27b0bb5fcdca1a86d26f0047c9b97779d8
rishabhdiwaker <rishabhdiwaker.rd@gmail.com>
2025-02-20 19:23:34 +0530

    model category updated

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,14 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionstring = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+        options.UseMySql(connectionstring, ServerVersion.AutoDetect(connectionstring)
+    });
 
 var app = builder.Build();
 
